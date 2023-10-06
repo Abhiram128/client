19a6248e479bcbb25525b373fdba6fb09bc77dca
Abhiram128 <90932429+Abhiram128@users.noreply.github.com>
2023-10-06 23:41:37 +0530

    Update useSignup.js

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -11,32 +11,61 @@ export const useSignup = () => {
     setLoading(true);
     setError(null);
 
-    const res = await fetch(
-      `${process.env.REACT_APP_BASE_URL}/api/user/signup`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
+    try {
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/user/signup`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email, password }),
+        }
+      );
+
+      const json = await res.json();
+
+      if (!res.ok) {
+        setLoading(false);
+        setError(json.error);
       }
-    );
 
-    const json = await res.json();
+      if (res.ok) {
+        // Registration was successful, but the user's email is not yet verified
+        // You should now initiate sending a verification email
+        await sendVerificationEmail(email); // Implement this function
 
-    if (!res.ok) {
+        // You can display a message to the user to check their email for verification
+        // This can be handled in your frontend component
+
+        setLoading(false);
+      }
+    } catch (error) {
+      setError("An error occurred during signup.");
       setLoading(false);
-      setError(json.error);
     }
+  };
 
-    if (res.ok) {
-      //  update the auth context
-      dispatch({ type: "LOGIN", payload: json });
+  // Implement a function to send a verification email
+  const sendVerificationEmail = async (email) => {
+    try {
+      // Make an API request to your backend to send a verification email
+      const res = await fetch(
+        `${process.env.REACT_APP_BASE_URL}/api/user/send-verification-email`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ email }),
+        }
+      );
 
-      // save the user to local storage
-      localStorage.setItem("user", JSON.stringify(json));
-
-      setLoading(false);
+      if (!res.ok) {
+        throw new Error("Failed to send verification email.");
+      }
+    } catch (error) {
+      console.error("Error sending verification email:", error);
     }
   };
 
